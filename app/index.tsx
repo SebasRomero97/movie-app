bfc606a5eede24c1fe17bceedefa1b58f94103ae
SebasRomero <sebas_3233@hotmail.com>
2025-05-22 20:24:33 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const RootApp = () => {
+  return (
+    <SafeAreaView>
+      <Text className='mt-10'>RootApp</Text>
+      <Text className='mt-10'>RootApp</Text>
+      <Text className='mt-10'>RootApp</Text>
+      <Text className='mt-10'>RootApp</Text>
+      <Text className='mt-10'>RootApp</Text>
+
+    </SafeAreaView>
+  )
+}
+
+export default RootApp
